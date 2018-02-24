555294b5d434b3b4cc9805b1209c54b55d34f04f
chaiguifeng <chaimouren2@163.com>
2018-02-24 17:35:27 +0800

    修改

--- music/urls.py
+++ music/urls.py
@@ -2,7 +2,10 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = 'music'
 urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    re_path(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+
 ]
