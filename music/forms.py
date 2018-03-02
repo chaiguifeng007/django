f5b124beb840ca4c47d11fd5cb6cb645552c70f3
chaiguifeng <chaimouren2@163.com>
2018-03-02 17:52:08 +0800

    urls

--- music/forms.py
+++ music/forms.py
@@ -2,9 +2,8 @@ from django.contrib.auth.models import User
 from django import forms
 
 class UserForm(forms.ModelForm):
-    passowrd = forms.CharField(widget=forms.PasswordInput)
-    # passowrd_confirm = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'passowrd']
+        fields = ['username', 'email', 'password']
--- music/urls.py
+++ music/urls.py
@@ -1,17 +1,16 @@
 #music/url
 from django.urls import path, re_path
 from . import views
-from .views import UserFormView
 
 app_name = 'music'
 urlpatterns = [
-    path('', views.as_view(), name='index'),
-    re_path(r'^register/$', UserFormView.as_view(), name='register'),
-    re_path(r'^(?P<pk>[0-9]+)/$', DetailView.as_view(), name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    re_path(r'^register/$', views.UserFormView.as_view(), name='register'),
+    re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # /music/album/add/
-    re_path(r'album/add/$', AlbumCreate.as_view(), name='album-add'),
+    re_path(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 
-    re_path(r'album/(?P<pk>[0-9]+)/$', AlbumUpdate.as_view(), name='album-update'),
-    re_path(r'album/(?P<pk>[0-9]+)/delete/$', AlbumDelete.as_view(), name='album-delete'),
+    re_path(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+    re_path(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 
 ]
